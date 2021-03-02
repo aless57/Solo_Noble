b686a9a5a2a57902a66a2da0501eb5358ca9d72c
Alessi <alessi.demange2@etu.univ-lorraine.fr>
2021-03-02 15:23:56 +0100

    Transfert de fichiers

---
+++ src/programme/SoloNoble.java
@@ -0,0 +1,286 @@
+package programme;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+/**
+ * classe gerant la resolution d un Solo Noble
+ * 
+ * @author DEMANGE Alessi - NICOL Benoit S3C
+ *
+ */
+public class SoloNoble {
+
+	/**
+	 * attribut donnant la grille du SoloNoble
+	 */
+	private Grille tablier;
+
+	/**
+	 * attribut rassemblant l'ensemble des grilles menant a la resolution du
+	 * SoloNoble
+	 */
+	private HashMap<Integer, String[][]> solutions;
+
+	/**
+	 * attribut donnant le nombre de deplacements realises durant la resolution
+	 */
+	private int nombreDeplacements;
+
+	/**
+	 * attribut donnant le nombre d appels de la methode resoudreSoloNoble()
+	 */
+	private int nombreAppelsResoudreSoloNoble;
+
+	/**
+	 * constructeur d un SoloNoble avec un nom de fichier en parametre
+	 * 
+	 * @param nomFichier nom de la grille a utiliser
+	 * @throws IOException en cas d erreur de lecture de fichier
+	 */
+	public SoloNoble(String nomFichier) throws IOException {
+
+		tablier = new Grille(nomFichier);
+		solutions = new HashMap<Integer, String[][]>();
+		nombreDeplacements = 0;
+		nombreAppelsResoudreSoloNoble = 0;
+
+		// Message de début
+		String s = "Bienvenue !\n";
+		s += "Cet algorithme va tenter de résoudre le Solo Noble suivant :\n\n";
+
+		// On affiche la grille
+		for (int i = 0; i < tablier.getGrille().length; i++) {
+			for (int j = 0; j < tablier.getGrille()[0].length; j++) {
+				s += tablier.getGrille()[i][j];
+			}
+			s += "\n";
+		}
+
+		System.out.println(s);
+	}
+
+	/**
+	 * constructeur d un SoloNoble par defaut
+	 * 
+	 * @throws IOException
+	 */
+	public SoloNoble() throws IOException {
+
+		tablier = new Grille();
+		solutions = new HashMap<Integer, String[][]>();
+		nombreDeplacements = 0;
+		nombreAppelsResoudreSoloNoble = 0;
+
+		// Message de début
+		String s = "Bienvenue !\n";
+		s += "Cet algorithme va tenter de résoudre le Solo Noble suivant :\n\n";
+
+		// On affiche la grille
+		for (int i = 0; i < tablier.getGrille().length; i++) {
+			for (int j = 0; j < tablier.getGrille()[0].length; j++) {
+				s += new String(tablier.getGrille()[i][j]);
+			}
+			s += "\n";
+		}
+
+		System.out.println(s);
+	}
+
+	/**
+	 * methode permettant de resoudre le SoloNoble a l aide de la recursivite avec
+	 * retour arriere
+	 * 
+	 * @param billes le nombre de billes restant dans la grille
+	 * @return true si on a reussi a resoudre le SoloNoble (si billes est egal a 1),
+	 *         false sinon
+	 */
+	public boolean resoudreSoloNoble(int billes) {
+
+		// Booléen initialisé à false
+		// Il sera vrai si et seulement si le nombre de billes est égal à 1
+		boolean grilleValide = false;
+
+		// On incremente le nombre d'appels de la méthode resoudreSoloNoble(billes)
+		nombreAppelsResoudreSoloNoble++;
+
+		// On stocke dans une variable la grille actuelle en copiant caractère par
+		// caractère
+		// Cela nous a permis de contourner le problème dû au stockage d'une grille
+		// Si on sauvegardait toute la grille directement, on aurait un problème car la
+		// grille change en permanence, à cause de la récursivité avec retour arrière
+		// L'ensemble des grilles contenues dans solutions seraient alors toutes les
+		// mêmes et contiendraient la grille de fin (avec une bille)
+		String[][] grilleTempo = new String[tablier.getNbLignes()][tablier.getNbColonnes()];
+		for (int i = 0; i < tablier.getGrille().length; i++) {
+			for (int j = 0; j < tablier.getGrille()[0].length; j++) {
+				grilleTempo[i][j] = tablier.getGrille()[i][j];
+			}
+		}
+
+		// on ajoute à la table solutions la grille, avec la clé billes
+		solutions.put(billes, grilleTempo);
+
+		if (billes == 1) {
+
+			// On a résolu le Solo Noble
+			grilleValide = true;
+
+			// On affiche toutes les solutions
+			ecrireToutesSolutions();
+		}
+
+		else {
+
+			// On change la valeur de l'attribut deplacement de la grille à debut
+			// Car on commence un nouvel appel de la fonction resoudreSoloNoble(billes)
+			tablier.setDeplacement("debut");
+
+			// On initialise deux indices afin de se déplacer dans la grille
+			int i = 0;
+			int j = 0;
+
+			// On continue tant que l'on a pas résolu et que l'on est pas arrivé à la fin de
+			// la grille
+			while (!grilleValide && i < tablier.getNbLignes()) {
+
+				j = 0;
+				while (!grilleValide && j < tablier.getNbColonnes()) {
+
+					// Si le caractère à la position [i,j] est un trou
+					if (tablier.getGrille()[i][j].equals(".")) {
+
+						// On tente d'effectuer un déplacement, défini selon un ordre précis
+						tablier.deplacerBille(i, j);
+
+						// On continue tant que l'on a pas résolu le Solo Noble
+						// Et que l'on est pas arrivé à la fin des déplacements possibles
+						while (!grilleValide && !tablier.getDeplacement().equals("fin")) {
+
+							// On incrémente le nombre de déplacements
+							nombreDeplacements++;
+
+							// On récupère le déplacement actuel
+							String deplacement = new String(tablier.getDeplacement());
+
+							// La variable grilleValide récupère le resultat de la fonction
+							// resoudreSoloNoble(billes - 1)
+							// C'est un appel caractéristique de la récursivité avec retour arrière
+							grilleValide = resoudreSoloNoble(billes - 1);
+
+							// Si notre grille n'était pas valide
+							if (!grilleValide) {
+
+								// On récupère l'ancien déplacement
+								tablier.setDeplacement(deplacement);
+
+								// On effectue le retour Arriere
+								tablier.retourArriere(i, j);
+
+								// On incrémente le nombre de déplacements
+								nombreDeplacements++;
+
+								// On teste un autre déplacement, si il y en a
+								tablier.deplacerBille(i, j);
+
+							}
+						}
+					}
+					j++;
+				}
+				i++;
+			}
+		}
+		return grilleValide;
+	}
+
+	/**
+	 * methode permettant d ecrire toutes les solutions et les messages donnant le
+	 * nombre de deplacements et d appels de la methode resoudreSoloNoble()
+	 */
+	public void ecrireToutesSolutions() {
+
+		System.out.println("\nLe Solo Noble est résolu !\n");
+		System.out.println("Voici les étapes menant à la résolution :\n");
+
+		// On parcourt la table solutions depuis la fin
+		for (int i = solutions.size(); i > 0; i--) {
+
+			// On récupère la grille associée
+			String[][] elt = solutions.get(i);
+
+			// On affiche le numéro de l'étape en fonction du nombre d'éléments de la table
+			// solutions
+			System.out.println("\nEtape n°" + (solutions.size() + 1 - i) + "\n");
+
+			// On parcourt la grille actuelle et on l'affiche
+			for (int j = 0; j < elt.length; j++) {
+
+				for (int k = 0; k < elt[0].length; k++) {
+
+					System.out.print(elt[j][k]);
+				}
+				System.out.println();
+			}
+			System.out.println("\n-------");
+		}
+
+		// On affiche le nombre de déplacements et le nombre
+		System.out.println("Nombre de déplacements : " + nombreDeplacements);
+		System.out.println("Nombre d'appels de la méthode resoudreSoloNoble() : " + nombreAppelsResoudreSoloNoble);
+	}
+
+	/**
+	 * methode main permettant de lancer la fonction resoudreSoloNoble() et d
+	 * afficher le temps d execution
+	 * 
+	 * @param args le nom de la grille si il y en a
+	 * @throws IOException en cas d erreur de lecture de fichier
+	 */
+	public static void main(String[] args) throws IOException {
+
+		// On crée un nouveau SoloNoble
+		SoloNoble sn;
+		if (args.length == 1)
+
+			// On utilise le constructeur SoloNoble(String) si il y a un argument
+			sn = new SoloNoble(args[0]);
+
+		// On utilise le constructeur SoloNoble() si il n'y a pas d'arguments
+		else
+			sn = new SoloNoble();
+		
+		int nbBilles = sn.getTablier().calculerNombreBilles();
+		
+		System.out.println("\nLe nombre de billes de cette grille est de " + nbBilles + "\n");
+		
+		// On cree une variable récupérant l'heure actuelle en nanosecondes
+		double debut = System.nanoTime();
+		
+		//On appelle la methode de resolution
+		boolean resolution = sn.resoudreSoloNoble(nbBilles);
+
+		// On cree une autre variable récupérant l'heure actuelle en nanosecondes
+		double fin = System.nanoTime();
+
+		// Si la résolution est impossible, cela veut dire que la grille ne pas valide
+		// Et on l'indique à l'utilisateur
+		if (!resolution)
+			System.out.println("Grille impossible !");
+
+		// On affiche le temps de recherche
+		// En faisant la différence de nos deux variables et en divisant le résultat par
+		// 1 000 000 000
+		// Afin d'obtenir le temps en secondes
+		System.out.println("Temps de recherche : " + ((fin - debut) / 1000000000) + "s");
+	}
+
+	/**
+	 * getter du tablier pour la methode main
+	 * 
+	 * @return le tablier du SoloNoble
+	 */
+	public Grille getTablier() {
+		return tablier;
+	}
+}
---
+++ src/programme/TestGrille.java
@@ -0,0 +1,325 @@
+package programme;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * classe gerant les mouvements dans la grille du SoloNoble
+ * @author DEMANGE Alessi - NICOL Benoit
+ *
+ */
+public class TestGrille {
+	
+	/**
+	 * attribut donnant le nombre de lignes de la grille
+	 */
+	private int nbLignes;
+	
+	/**
+	 * attribut donnant le nombre de colonnes de la grille
+	 */
+	private int nbColonnes;
+	
+	/**
+	 * attribut donnant la grille evaluee
+	 */
+	private String[][] grille;
+	
+	/**
+	 * attribut donnant le dernier deplacement effectue pour un trou donne, debut si il n y en a pas encore eu, fin si on a teste tous les deplacements
+	 */
+	private String deplacement;
+	
+	/**
+	 * constructeur d une Grille avec un nom de fichier en parametre
+	 * @param nomFichier le nom de la grille a evaluer
+	 * @throws IOException en cas d erreur de lecture de fichier
+	 */
+	public TestGrille(String nomFichier) throws IOException {
+		
+		//On crée un BufferedReader qui va lire le fichier
+		BufferedReader br = new BufferedReader(new FileReader(new File(nomFichier)));
+		
+		//On crée une variable ligne qui va contenir chaque ligne du fichier
+		String ligne;
+		
+		//On crée une liste de listes de String afin de pouvoir instancier la grille
+		//Cela nous sert aussi à stocker chaque élément de la liste dans la grille
+		List<List<String>> l1 = new ArrayList<List<String>>();
+		
+		//Tant que l'on est pas arrivé à la fin du fichier
+		//La variable ligne est actualisée
+		while ((ligne = br.readLine()) != null){
+			
+			//On crée une liste de String
+			List<String> l2 = new ArrayList<String>();
+			
+			//On ajoute chaque élément de la ligne à la deuxième liste
+			for (int j = 0; j < ligne.length(); j++) {
+				l2.add(Character.toString(ligne.charAt(j)));
+			}
+			
+			//On ajoute la deuxième liste à la première
+			l1.add(l2);
+		}
+		
+		//On ferme le BufferedReader
+		br.close();
+		
+		//On instancie la grille
+		this.grille = new String[l1.size()][l1.get(0).size()];
+		
+		//On stocke chaque caractère de la liste dans la grille
+		for (int i = 0; i < l1.size(); i++) {
+            for (int j = 0; j < l1.get(0).size(); j++) {
+                grille[i][j] = l1.get(i).get(j);
+            }
+        }
+		
+		//On initialise le nombre de colonnes, de lignes et de déplacements
+		nbColonnes = grille[0].length;
+		nbLignes = grille.length;
+		deplacement = "debut";
+	}
+	
+	/**
+	 * constructeur d une Grille par defaut avec le tablier1
+	 * @throws IOException
+	 */
+	public TestGrille() throws IOException {
+		
+		//On instancie la grille
+		grille = new String[7][7];
+		
+		//On effectue cette opération pour les 2 premières lignes
+		for (int i = 0; i < 2; i++) {
+			grille[i][0] = " ";
+			grille[i][1] = " ";
+			for (int j = 2; j < 5; j++) {
+				grille[i][j] = "o";
+			}
+			grille[i][5] = " ";
+			grille[i][6] = " ";
+		}
+		
+		//On effectue cette opération pour les lignes 2, 3 et 4
+		for (int i = 2; i < 5; i++) {
+			for (int j = 0; j < grille[0].length; j++) {
+				grille[i][j] = "o";
+			}
+		}
+		//On crée l'unique trou au centre de la grille
+		grille[3][3] = ".";
+		
+		//On effectue cette opération pour les 2 dernières lignes
+		for (int i = 5; i < grille.length; i++) {
+			grille[i][0] = " ";
+			grille[i][1] = " ";
+			for (int j = 2; j < 5; j++) {
+				grille[i][j] = "o";
+			}
+			grille[i][5] = " ";
+			grille[i][6] = " ";
+		}
+		
+		//On initialise le nombre de colonnes, de lignes et de déplacements
+		nbColonnes = grille[0].length;
+		nbLignes = grille.length;
+		deplacement = "debut";
+	}
+	
+	/**
+	 * methode qui calcule le nombre de billes de la grille
+	 * @return
+	 */
+	public int calculerNombreBilles() {
+		
+		//On crée un compteur
+		int somme = 0;
+		
+		//On parcourt la grille
+		for (int i = 0; i < nbLignes; i++) {
+			for (int j = 0; j < nbColonnes; j++) {
+				
+				//Si le caractère est une bille, on incrément le compteur
+				if(grille[i][j].equals("o"))
+					somme++;
+			}
+		}
+		
+		//On retourne le compteur
+		return somme;
+	}
+	
+	/**
+	 * methode gerant le deplacement de la bille selon un certain ordre
+	 * cette methode verifie egalement si le deplacement est possible
+	 * on utilise l attribut deplacement afin de savoir si on a deja effectue tel ou tel deplacement pour un trou donne
+	 * 
+	 * @param i ligne du trou
+	 * @param j colonne du trou
+	 */
+	public void deplacerBille(int i, int j){
+		
+		//booléen initialisé à false qui vaut true si un déplacement a déjà été effectué
+		boolean deplacementEffectue = false;
+		
+		//Si on est arrivé à la fin au dernier appel de la méthode, alors on actualise l'attribut déplacement à "debut"
+		if(deplacement.equals("fin"))
+			deplacement = "debut";
+		
+		//On effectue d'abord le déplacement à gauche
+		//On teste si on est au début et que le déplacement est valide
+		if (deplacement.equals("debut") && (i + 2) < nbLignes) {
+			
+			if(grille[i+1][j].equals("o") && grille[i+2][j].equals("o")) {
+				
+				grille[i][j] = "o";
+				grille[i+1][j] = ".";
+				grille[i+2][j] = ".";
+				
+				deplacement = "haut";
+				deplacementEffectue = true;
+			}
+		}
+		
+		//On effectue ensuite le déplacement vers la gauche
+		//Seulement si on a pas effectué de déplacement durant cet appel de méthode
+		//Et qu'on n'a pas encore effectué de déplacement pour ce trou
+		//Ou si on a effectué le déplacement vers le haut pour ce trou
+		//Et si le déplacement est valide
+		if(!deplacementEffectue && (deplacement.equals("debut") || deplacement.equals("haut")) && (j+2) < nbColonnes) {
+			
+			if(grille[i][j+1].equals("o") && grille[i][j+2].equals("o")) {
+				
+				grille[i][j] = "o";
+				grille[i][j+1] = ".";
+				grille[i][j+2] = ".";
+				
+				deplacement = "gauche";
+				deplacementEffectue = true;
+			}
+		}
+		
+		//On effectue ensuite le déplacement vers la droite
+		//Seulement si on a pas effectué de déplacement durant cet appel de méthode
+		//Et qu'on n'a pas effectué de déplacement vers la droite pour ce trou
+		//Et qu'on n'a pas effectué de déplacement vers le bas pour ce trou
+		//Et que le déplacement est valide
+		if (!deplacementEffectue && !deplacement.equals("droite") && !deplacement.equals("bas") && (j-2)>=0) {
+			
+			if(grille[i][j-1].equals("o") && grille[i][j-2].equals("o")) {
+				
+				grille[i][j-2] = ".";
+				grille[i][j-1] = ".";
+				grille[i][j] = "o";
+				
+				deplacement = "droite";
+				deplacementEffectue = true;
+			}
+		}
+		
+		//On effectue enfin le déplacement vers le bas
+		//Seulement si on a pas effectué de déplacement durant cet appel de méthode
+		//Et qu'on n'a pas effectué de déplacement vers le bas pour ce trou
+		//Et que le déplacement est valide
+		if (!deplacementEffectue && !deplacement.equals("bas") && (i-2)>= 0) {
+			
+			if(grille[i-2][j].equals("o") && grille[i-1][j].equals("o")) {
+				
+				grille[i-2][j] = ".";
+				grille[i-1][j] = ".";
+				grille[i][j] = "o";
+				
+				deplacement = "bas";
+				deplacementEffectue = true;
+			}
+		}
+		
+		//Si aucun déplacement n'a été possible durant cet appel, on actualise le déplacement à "fin"
+		//Ce qui permet d'arrêter la boucle while dans la méthode resoudreSoloNoble()
+		if (!deplacementEffectue)
+			deplacement = "fin";
+	}
+	
+	/**
+	 * methode permettant d effectuer le deplacement inverse pour la grille, en fonction du deplacement que l on vient d effectuer
+	 * @param i la ligne du trou
+	 * @param j la colonne du trou
+	 */
+	public void retourArriere(int i, int j) {
+		
+		switch(deplacement) {
+		
+		case "gauche" :
+			grille[i][j] = ".";
+			grille[i][j+1] = "o";
+			grille[i][j+2] = "o";
+			break;
+			
+		case "droite" :
+			grille[i][j-2] = "o";
+			grille[i][j-1] = "o";
+			grille[i][j] = ".";
+			break;
+			
+		case "bas" :
+			grille[i-2][j] = "o";
+			grille[i-1][j] = "o";
+			grille[i][j] = ".";
+			break;
+			
+		case "haut" :
+			grille[i][j] = ".";
+			grille[i+1][j] = "o";
+			grille[i+2][j] = "o";
+			break;
+		}
+	}
+	
+	/**
+	 * getter de la grille
+	 * @return la grille
+	 */
+	public String[][] getGrille() {
+		return grille;
+	}
+	
+	/**
+	 * getter du nombre de lignes
+	 * @return le nombre de lignes
+	 */
+	public int getNbLignes() {
+		return nbLignes;
+	}
+	
+	/**
+	 * getter du nombre de colonnes
+	 * @return le nombre de colonnes
+	 */
+	public int getNbColonnes() {
+		return nbColonnes;
+	}
+	
+	/**
+	 * getter du dernier deplacement effectue
+	 * @return le deplacement
+	 */
+	public String getDeplacement() {
+		return deplacement;
+	}
+	
+	/**
+	 * setter du deplacement
+	 * @param deplacement le nouveau deplacement
+	 */
+	public void setDeplacement(String deplacement) {
+		this.deplacement = deplacement;
+	}
+	
+	
+}
